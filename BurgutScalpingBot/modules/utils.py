f4750ebc0e9a668b62320f7831fcbcf652a41199
burgutbey9 <burgutbey9@gmail.com>
2025-07-04 06:44:35 +0500

    Create utils.py

---
+++ BurgutScalpingBot/modules/utils.py
@@ -0,0 +1,10 @@
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram_status(message: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+    requests.post(url, json=payload)
