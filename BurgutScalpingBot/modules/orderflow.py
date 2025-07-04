79f2ee2beda251f8c7c72e0d2b6b93f1d3260eab
burgutbey9 <burgutbey9@gmail.com>
2025-07-04 06:44:08 +0500

    Create orderflow.py

---
+++ BurgutScalpingBot/modules/orderflow.py
@@ -0,0 +1,6 @@
+import asyncio
+
+async def run_orderflow():
+    print("Order Flow kuzatuvchi modul ishladi...")
+    # Bu yerda on-chain eventlar, Mempool, Volume imbalance kuzatiladi.
+    # Keyinchalik bu ma’lumot asosida AI qaror qabul qiladi.
